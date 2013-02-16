19c607e10c5da3705f4a482c43128e1d86c7a739
novohispano@gmail.com <novohispano@gmail.com>
2013-02-15 21:27:24 -0700

    Invoice class fixed

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,7 +1,12 @@
 require "csv"
 
 class Invoice
-  attr_reader :id, :customer_id, :merchant_id, :status, :created_at, :updated_at
+  attr_reader :id
+  attr_reader :customer_id
+  attr_reader :merchant_id
+  attr_reader :status
+  attr_reader :created_at
+  attr_reader :updated_at
 
   def initialize(data)
     @id = data["id"]
@@ -31,31 +36,51 @@ class Invoice
     @invoices
   end
 
-  def self.find_by_customer_id(customer_id)
-    @invoices.find{|invoice| invoice.customer_id == customer_id}
-  end
-
-  def self.find_all_by_customer_id(customer_id)
-    @invoices.select{|invoice| invoice.customer_id == customer_id}
-  end  
-
-  def self.find_by_merchant_id(merchant_id)
-    @invoices.find{|invoice| invoice.merchant_id == merchant_id}
-  end
-
   def self.find_by_id(id)
     @invoices.find{|invoice| invoice.id == id}
   end
 
-  def self.find_all_by_merchant_id(merchant_id)
-    @invoices.select{|invoice| invoice.merchant_id == merchant_id}
+  def self.find_by_customer_id(customer_id)
+    @invoices.find{|invoice| invoice.customer_id == customer_id}
+  end
+
+  def self.find_by_merchant_id(merchant_id)
+    @invoices.find{|invoice| invoice.merchant_id == merchant_id}
   end
 
   def self.find_by_status(status)
     @invoices.find{|invoice| invoice.status == status}
   end
 
+  def self.find_by_created_at(created_at)
+    @invoices.find{|invoice| invoice.created_at == created_at}
+  end
+
+  def self.find_by_updated_at(updated_at)
+    @invoices.find{|invoice| invoice.updated_at == updated_at}
+  end
+
+  def self.find_all_by_id(id)
+    @invoices.select{|invoice| invoice.id == id}
+  end
+
+  def self.find_all_by_customer_id(customer_id)
+    @invoices.select{|invoice| invoice.customer_id == customer_id}
+  end  
+
+  def self.find_all_by_merchant_id(merchant_id)
+    @invoices.select{|invoice| invoice.merchant_id == merchant_id}
+  end
+
   def self.find_all_by_status(status)
     @invoices.select{|invoice| invoice.status == status}
+  end
+
+  def self.find_all_by_created_at(created_at)
+    @invoices.select{|invoice| invoice.created_at == created_at}
+  end
+
+  def self.find_all_by_updated_at(updated_at)
+    @invoices.select{|invoice| invoice.updated_at == updated_at}
   end
 end
